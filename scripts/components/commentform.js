14112f75842b4838fc18e97dfa464a3670dc8dac
Kristen Rieger <karieger1@gmail.com>
2015-06-15 15:05:37 -0500

    initial commit with everything hooked up

---
+++ scripts/components/commentform.js
@@ -0,0 +1,27 @@
+var React = require('react');
+var CommentModel = require('../models/commentmodel');
+
+module.exports = React.createClass({
+	render: function() {
+		return (
+			<form onSubmit={this.commentSubmitted}>
+				<input ref="commentText" type="text" placeholder="Enter a comment..." />
+				<button type="submit">Add Comment</button>
+			</form>
+		);
+	},
+	commentSubmitted: function(e) {
+		e.preventDefault();
+		var comment = new CommentModel({
+			text: this.refs.commentText.getDOMNode().value,
+			createdAt: new Date()
+		});
+		
+		if(!comment.isValid()) {
+			console.log(comment.validationError);
+		}
+		else {
+			console.log(comment);
+		}
+	}
+});
---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+var React = require('react');
+var CommentForm = require('./components/commentform');
+
+React.render(
+	<CommentForm />,
+	document.getElementById('container')
+);
