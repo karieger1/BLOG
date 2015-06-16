fabb2df2086cdc595bdc00dfd89312f3f4a30baf
Kristen Rieger <karieger1@gmail.com>
2015-06-15 20:05:37 -0500

    everything linked

---
+++ scripts/components/commentlist.js
@@ -0,0 +1,12 @@
+var react = require("react");
+var CommentModel = require ("./models/commentmodel.js");
+var CommentCollection = require ("./collections/commentcollection.js");
+commentBoard = CommentCollection;
+
+module.exports = React.createClass({
+	// render: function() {
+	// 	var commentElements = this.props.allComments.map(function(CommentModel)) {
+	// 		//return 
+	// 	}
+	// }
+})
--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,12 @@
 var React = require('react');
 var CommentForm = require('./components/commentform');
+var CommentList = require("./components/commentlist");
+var CommentCollection = require("./collections/commentcollection");
+var comments = require("./components/commentcomponent");
 
 React.render(
-	<CommentForm />,
+	<div>
+		<CommentForm />
+	</div>,
 	document.getElementById('container')
 );
